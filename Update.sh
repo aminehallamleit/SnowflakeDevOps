f412c127b730e507037902a9813d7eccc7ae8be9
amhallam <amine.hallam.uk@gmail.com>
2021-03-25 17:06:55 +0000

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,4 +1,10 @@
 #/bin/bash
 
+
+#you can pass variables from octopus to this file, and use sed -i to apply the changes on the liquibase.properties file before running the docker command below
+#i.e USERNAME = $PARAM1 
+#sed -e 's/USERNAME/$PARAM1/' Liquibase/liquibase.properties
+
+
 echo 'Updates database to current version.'
 docker run --rm -v $(pwd)/Liquibase:/liquibase/changelog liquibase/liquibase --defaultsFile=/liquibase/changelog/liquibase.properties update
